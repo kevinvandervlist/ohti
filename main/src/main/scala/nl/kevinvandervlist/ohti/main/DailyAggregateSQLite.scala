5eec9dff979e6c1b9f8e431a7dc1ae8117f89501
Kevin van der Vlist <kevin@kevinvandervlist.nl>
2020-04-23 17:32:50 +0200

    Extract periodic aggregate + repo into separate class

--- main/src/main/scala/nl/kevinvandervlist/ohti/main/DailyAggregateSQLite.scala
+++ main/src/main/scala/nl/kevinvandervlist/ohti/main/DailyAggregateSQLite.scala
@@ -1,20 +1,17 @@
 package nl.kevinvandervlist.ohti.main
 
 import java.time.LocalDate
-import java.util.UUID
 
 import com.typesafe.scalalogging.LazyLogging
 import nl.kevinvandervlist.ohti.api.PortalAPI
-import nl.kevinvandervlist.ohti.api.model.{IthoZonedDateTime, MonitoringData}
+import nl.kevinvandervlist.ohti.api.model.IthoZonedDateTime
 import nl.kevinvandervlist.ohti.config.Settings
 import nl.kevinvandervlist.ohti.repository.PeriodicUsageRepository
-import nl.kevinvandervlist.ohti.repository.data.{PeriodicUsage, TimeSpan}
-import nl.kevinvandervlist.ohti.usage.{Devices, KnownDevices, RetrieveScenario, RetrieveTotal}
+import nl.kevinvandervlist.ohti.usage.{Devices, KnownDevices, PeriodicAggregateSQLite, RetrieveScenario, RetrieveTotal}
 
 import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{Await, ExecutionContext}
 import scala.language.postfixOps
-import scala.util.{Failure, Success}
 
 object DailyAggregateSQLite extends RunnableTask with KnownDevices with LazyLogging {
   override def name: String = "daily-aggregate-sqlite"
@@ -22,32 +19,17 @@ object DailyAggregateSQLite extends RunnableTask with KnownDevices with LazyLogg
   override def apply(api: PortalAPI, settings: Settings)(implicit ec: ExecutionContext): Unit = {
     val cfg = settings.taskConfig(name)
     val devs: Devices = devices(cfg)
+    val repo = PeriodicUsageRepository(cfg.getString("database"))
+    val agg = new PeriodicAggregateSQLite(repo, devs)
 
     val yesterday = IthoZonedDateTime
       .fromLocalDate(LocalDate.now.minusDays(1))
       .startOfDay
 
-    val cases = Set(
-      RetrieveScenario(yesterday.asPortalString.splitAt(10)._1, api.retrieveDailyData(_, yesterday), yesterday.startOfDay, yesterday.endOfDay),
-    )
-    val ts = TimeSpan(yesterday.startOfDay.asTimeStamp, yesterday.endOfDay.asTimeStamp)
-    val repo = PeriodicUsageRepository(cfg.getString("database"))
+    val scn = RetrieveScenario(yesterday.asPortalString.splitAt(10)._1, api.retrieveDailyData(_, yesterday), yesterday.startOfDay, yesterday.endOfDay)
 
-    repo.exists(ts) match {
-      case Failure(ex) => logger.error("The repository failed: {}", ex)
-      case Success(true) => logger.info(s"Daily aggregate for {} already exists", ts)
-      case Success(false) =>
-        logger.info("Aggregated statistics for timespan {} do not exist yet, retrieving them.", ts)
-        val maxDuration = 10000 millis
-        val info = Await.result(new RetrieveTotal(cases, devs).fetch(), maxDuration).head
-
-        repo.put(info) match {
-          case Success(i) => logger.info(s"Persisted daily aggregate {}", i.name)
-          case Failure(ex) => logger.error("Unable to persist daily aggregate: {}", ex)
-        }
-    }
-
-    logger.info("Daily aggregate: {}", repo.get(ts).get)
+    val maxDuration = 10000 millis
+    val pu = Await.result(agg.aggregate(scn), maxDuration)
+    logger.info("Daily aggregate: {}", pu)
   }
 }
-
---
+++ main/src/main/scala/nl/kevinvandervlist/ohti/usage/PeriodicAggregateSQLite.scala
@@ -0,0 +1,36 @@
+package nl.kevinvandervlist.ohti.usage
+
+import com.typesafe.scalalogging.LazyLogging
+import nl.kevinvandervlist.ohti.repository.PeriodicUsageRepository
+import nl.kevinvandervlist.ohti.repository.data.{PeriodicUsage, TimeSpan}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
+import scala.util.{Failure, Success}
+
+class PeriodicAggregateSQLite(repo: PeriodicUsageRepository, devices: Devices)(implicit ec: ExecutionContext) extends LazyLogging {
+
+  def aggregate(scenario: RetrieveScenario): Future[PeriodicUsage] = {
+    val ts = TimeSpan(scenario.start.asTimeStamp, scenario.end.asTimeStamp)
+
+    repo.get(ts) match {
+      case Failure(ex) =>
+        logger.error("The repository failed: {}", ex)
+        Future { ??? }
+      case Success(Some(pu)) =>
+        logger.info(s"Aggregate for {} already exists", ts)
+        Future { pu }
+      case Success(None) =>
+        logger.info("Aggregated statistics for timespan {} do not exist yet, retrieving them.", ts)
+        RetrieveTotal(scenario, devices).fetch()
+          .map(_.head)
+          .andThen {
+            case Success(pu: PeriodicUsage) => repo.put(pu) match {
+              case Success(i) => logger.info(s"Persisted daily aggregate {}", i.name)
+              case Failure(ex) => logger.error("Unable to persist daily aggregate: {}", ex)
+            }
+          }
+    }
+  }
+}
+
